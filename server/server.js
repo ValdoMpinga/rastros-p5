a75531091f25b81faddf7a0f7c846162e735cb1e
Valdo Mpinga <valdompinga2@gmail.com>
2024-04-04 14:36:48 +0100

    server served game

---
+++ server/server.js
@@ -0,0 +1,67 @@
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const path = require('path');
+
+const PORT = process.env.PORT || 3003;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let connectedClients = 0;
+
+app.use(cors());
+
+app.use((req, res, next) =>
+{
+    if (connectedClients < 2)
+    {
+        express.static(path.join(__dirname, '../public'))(req, res, next);
+    } else
+    {
+        res.status(403).send('Too many connected clients');
+    }
+});
+
+app.get('/', (req, res) =>
+{
+    console.log('Request received for /');
+    res.send('Hello World');
+});
+
+io.on('connection', (socket) =>
+{
+    connectedClients++;
+    console.log('A user connected. Total connected clients:', connectedClients);
+
+    io.emit('join', `Player ${connectedClients}`);
+
+
+    socket.on('disconnect', () =>
+    {
+        connectedClients--;
+        console.log('A user disconnected. Total connected clients:', connectedClients);
+    });
+
+    socket.on('move-piece', (data) =>
+    {
+        console.log("Piece moved to: " + data);
+        io.emit('move-piece', data);
+    });
+
+
+    socket.on('switch-turn', () =>
+    {
+        console.log("Emmiting switch turn");
+        io.emit('switch-turn');
+    });
+
+
+});
+
+server.listen(PORT, () =>
+{
+    console.log(`Server is running on port ${PORT}`);
+});
